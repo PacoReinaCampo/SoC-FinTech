94ba6bdff1624c0adaa00e394336809fd1982e09
PacoReinaCampo <frareicam@gmail.com>
2022-08-16 07:46:06 +0200

    Working in SoC-FinTech 🦀

--- doc/write.sh
+++ doc/write.sh
@@ -1,5 +1,8 @@
 rm -f *.pdf
 rm -f *.tex
 
+pandoc PRELUDE-TT.md -s -o SoC-TT-NTM.pdf
 pandoc BOOK.md -s -o SoC-FinTech.pdf
+
+pandoc PRELUDE-TT.md -s -o PU-TT-NTM.tex
 pandoc BOOK.md -s -o SoC-FinTech.tex
